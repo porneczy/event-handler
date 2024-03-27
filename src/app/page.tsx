f125050e1b0d7186a731cb27997c5f4763924f1d
porneczy <porneczy@gmail.com>
2024-03-27 14:37:40 +0100

    setup mui with example table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import EventListTable from "./ui/eventListTable";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>table</h1>
+      <EventListTable />
     </main>
   );
 }
